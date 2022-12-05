2f763aaff8d8f37056aa2a51b7ec92df937985f3
NguyenVanPhuocHau-19130066 <92570580+NguyenVanPhuocHau-19130066@users.noreply.github.com>
2022-12-05 21:54:02 +0700

    fix

--- src/src copy/pages/ForgotPassword/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './ForgotPassword';
-export * from './ForgotForm/ForgotInfo';
-export * from './ForgotForm/ForgotCode';
-export * from './ForgotForm/ForgotNewPassword';
--- src/src copy/pages/SignUpOwner/Form/FormSignUpOwner.js
+++
@@ -1,9 +0,0 @@
-function FormSignUpOwner({ children }) {
-    return (
-        <form action="#" method="post" className="grid">
-            {children}
-        </form>
-    );
-}
-
-export default FormSignUpOwner;
