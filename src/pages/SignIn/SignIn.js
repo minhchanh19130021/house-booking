d61e8886bf7706ad7c9016df14a4b6913b04a538
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2022-12-11 22:26:25 +0700

    đăng nhập google

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -7,8 +7,10 @@ import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { loginFailure, loginStart, loginSuccess } from '~/redux/authenticationSlide';
 import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
 
 const cx = classNames.bind(styles);
 function SignIn() {
@@ -55,6 +57,52 @@ function SignIn() {
             })();
         },
     });
+    // const [userInfoGoogle, setUserInfoGoogle] = useState();
+    // const login = useGoogleLogin({
+    //     onSuccess: async (tokenResponse) => {
+    //         const userInfo = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+    //             headers: { Authorization: `Bearer ${tokenResponse?.access_token}` },
+    //         });
+    //         dispatch(loginStart());
+    //         console.log(userInfoGoogle);
+    //         const ress = await fetch(`http://localhost:8080/api/v1/login-google`, {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 email: userInfo?.data.email,
+    //                 firstname: userInfo?.data.family_name,
+    //                 lastname: userInfo?.data.given_name,
+    //                 username: userInfo?.data.email.split('@')[0],
+    //                 picture: userInfo?.data.picture,
+    //             }),
+    //             headers: {
+    //                 'Content-type': 'application/json; charset=UTF-8',
+    //             },
+    //         })
+    //             .then((response) => response.json())
+    //             .then((data) => {
+    //                 console.log(data);
+    //             })
+    //             .catch((err) => console.log(err));
+
+    //         // await axios({
+    //         //     method: 'POST',
+    //         //     url: 'http://localhost:8080/api/v1/login-google',
+    //         //     data: {
+    //         //         email: userInfoGoogle?.data.email,
+    //         //         firstname: userInfoGoogle?.data.family_name,
+    //         //         lastname: userInfoGoogle?.data.given_name,
+    //         //         username: userInfoGoogle?.data.email.split('@')[0],
+    //         //         picture: userInfoGoogle?.data.picture,
+    //         //     },
+    //         // }).then((response) => {
+    //         //     console.log(response);
+    //         //     dispatch(loginSuccess(response.data.user));
+    //         //     navigate('/');
+    //         // });
+    //         // console.log(userInfo);
+    //     },
+    //     onError: (errorResponse) => console.log(errorResponse),
+    // });
 
     return (
         <div className={cx('wrapper')}>
@@ -107,8 +155,43 @@ function SignIn() {
                         </div>
                     </div>
                     <div className={cx('login-social')}>
-                        <Button className={cx('btn-social', 'facebook')}>Facebook</Button>
-                        <Button className={cx('btn-social', 'google')}>Google</Button>
+                        {/* <Button className={cx('btn-social', 'facebook')}>Facebook</Button> */}
+                        {/* <Button className={cx('btn-social', 'google')}>Google</Button> */}
+                    </div>
+                    <div className="grid">
+                        <div className="row">
+                            <div className="col l-6 m-12 c-12">
+                                <GoogleLogin
+                                    onSuccess={(credentialResponse) => {
+                                        dispatch(loginStart());
+                                        axios({
+                                            method: 'POST',
+                                            url: 'http://localhost:8080/api/v1/login-google',
+                                            data: { idToken: credentialResponse.credential },
+                                        }).then((response) => {
+                                            dispatch(loginSuccess(response.data.user));
+                                            navigate('/');
+                                        });
+                                    }}
+                                    onError={() => {
+                                        console.log('Login Failed');
+                                        dispatch(loginFailure());
+                                    }}
+                                    useOneTap
+                                />
+                            </div>
+                            <div className="col l-6 n-12 c-12">
+                                {/* <FacebookLogin
+                                    appId="1088597931155576"
+                                    autoLoad={true}
+                                    fields="name,email,picture"
+                                    onClick={componentClicked}
+                                    callback={responseFacebook}
+                                    cssClass="btn-social"
+                                    icon="fa-facebook"
+                                /> */}
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
